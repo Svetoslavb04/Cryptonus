f8d041ad056b03a2d5a82e721c1af29cf26cf485
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-02 23:28:26 +0300

    Added Tailwind

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,11 @@ import './App.css';
 function App() {
   return (
     <>
+      <div className='w-2/4 h-40 bg-surface dark'>
+        <h1 className="text-onsurface text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
 
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#FFD369',
+        seconday: '#EEEEEE',
+        background: '#040303',
+        surface: '#191919',
+        onsurface: colors.white
+      }
     },
   },
   plugins: [],
