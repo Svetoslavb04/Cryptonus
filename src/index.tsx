79f52b073320eb8a8d23b3f0937319fce599e502
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-04 19:24:53 +0300

    Included fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './fonts/Montserrat/Montserrat-Italic-VariableFont_wght.ttf';
+import './fonts/Montserrat/Montserrat-Italic-VariableFont_wght.ttf';
+import './fonts/Orbitron/Orbitron-VariableFont_wght.ttf';
+
 import './index.css';
 import App from './Components/App';
 
