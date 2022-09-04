371582081da132c9ac73c1541a0781cd3bfb70ea
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-04 19:25:33 +0300

    Added main Content Component

---
+++ src/Components/Core/Content/Content.tsx
@@ -0,0 +1,16 @@
+import Header from "../Header/Header"
+
+type Props = {
+    theme: string,
+    setTheme: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Content: React.FC<Props> = ({ theme, setTheme }) => {
+    return (
+        <div id='content' className='content bg-primary dark:bg-primaryDark min-h-screen transition-colors duration-[400ms]'>
+            <Header theme={theme} setTheme={setTheme} />
+        </div>
+    )
+}
+
+export default Content
