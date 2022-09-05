643d4ff55db7cd4b1051e76c0ee5223ffcd6d7b1
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-09-05 22:42:22 +0300

    Added Footer

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <div id='cryptonus' className={`cryptonus overflow-hidden ${theme}`}>
+      <div id='cryptonus' className={`cryptonus overflow-hidden relative ${theme}`}>
         <Content theme={theme} setTheme={setTheme} />
       </div>
     </>
---
+++ src/Components/Core/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <div className="footer p-5 w-full justify-center absolute border-t border-primaryDark-light bottom-0 flex bg-primary dark:bg-primaryDark-dark dark:text-onPrimaryDark text-onPrimary text-sm">
+        ©2022 Cryptonus All Rights Reserved. Privacy Terms
+    </div>
+  )
+}
+
+export default Footer
