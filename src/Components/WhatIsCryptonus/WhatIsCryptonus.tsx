d89e7f19b64333709f326b71dd94b761ba4ca25c
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-10-08 23:16:07 +0300

    Excluded Sections from Home

---
+++ src/Components/WhatIsCryptonus/WhatIsCryptonus.tsx
@@ -0,0 +1,38 @@
+import { FC, useRef } from 'react'
+import { useScrollTransition } from '../../hooks/useScrollTransition';
+
+import { ChevronRight } from 'react-feather';
+
+const WhatIsCryptonus: FC = () => {
+
+    const whatIsCryptonusRef = useRef<HTMLDivElement>(null);
+
+    useScrollTransition(whatIsCryptonusRef, 50);
+
+    return (
+        <section id='what-is-cryptonus' className='my-8 mx-2 sm:mx-6 md:mx-12 bg-primary-dark/10 dark:bg-primaryDark-dark rounded-xl md:py-4  xl:py-16 px-6'>
+            <div className='flex gap-16 flex-col xl:flex-row'>
+                <div className='basis-2/5'>
+                    <img src="assets/images/crypto-home-globe.png" alt="" className='m-auto w-[70%] xl:w-fit xl:max-h-[650px]' />
+                </div>
+                <div ref={whatIsCryptonusRef} className='basis-3/5 flex flex-col on-scroll-transition'>
+                    <h2 className='text-2xl xs:text-3xl sm:text-5xl md:text-6xl'>What is Cryptonus</h2>
+                    <p className='xs:text-md sm:text-xl md:text-2xl opacity-85 my-6'>Receive the best trading experience with Cryptonus. Many features are available for you. Start now and receive a bonus!</p>
+                    {
+                        [
+                            'Access real-time cryptocurrency prices',
+                            'Buy & Sell the most popular cryptocurrencies',
+                            'P2P trading available',
+                            '24/7 Customer support'
+                        ].map(text =>
+                        (
+                            <div key={text} className='flex items-center xs:text-xl  sm:text-2xl md:text-3xl my-4'><ChevronRight size={40} />{text}</div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default WhatIsCryptonus
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,52 +1,16 @@
-import './Home.css'
-import { RefObject, useCallback, useEffect, useRef } from 'react'
-import CryptoCard from '../../Components/CryptoCard/CryptoCard';
-import { ChevronRight } from 'react-feather';
+import './Home.css';
+
+import { useRef } from 'react';
+import { useScrollTransition } from '../../hooks/useScrollTransition';
+
+import WhatIsCryptonus from '../../Components/WhatIsCryptonus/WhatIsCryptonus';
+import CurrenciesSection from '../../Components/CurrenciesSection/CurrenciesSection';
 
 const Home: React.FC = () => {
 
   const currenciesSection = useRef<HTMLElement>(null);
-  const whatIsCryptonusRef = useRef<HTMLDivElement>(null);
 
-  const windowScrollHandler = useCallback(() => {
-
-    showOnScrollElement(currenciesSection, 60);
-    showOnScrollElement(whatIsCryptonusRef, 70);
-
-  }, [currenciesSection, whatIsCryptonusRef]);
-
-  useEffect(() => {
-
-    window.addEventListener('scroll', windowScrollHandler);
-
-    return () => {
-
-      window.removeEventListener('scroll', windowScrollHandler);
-
-    }
-
-  }, [windowScrollHandler]);
-
-
-  function showOnScrollElement(elementRef: RefObject<HTMLElement>, elementVisibleAfterPixels: number) {
-
-    const windowHeight = window.innerHeight;
-
-    const elementTop = elementRef.current?.getBoundingClientRect().top;
-
-    if ((elementTop || Number.MAX_SAFE_INTEGER) < windowHeight - elementVisibleAfterPixels) {
-
-      elementRef.current?.classList.add("show-on-scroll");
-      elementRef.current?.classList.remove("hide-on-scroll");
-
-    } else {
-
-      elementRef.current?.classList.remove("show-on-scroll");
-      elementRef.current?.classList.add("hide-on-scroll");
-
-    }
-
-  }
+  useScrollTransition(currenciesSection, 60);
 
   return (
     <div className="home-page dark:text-onPrimaryDark text-onPrimary p-2">
@@ -65,67 +29,8 @@ const Home: React.FC = () => {
           <img src="assets/images/cryptocurrency2.png" alt="" className='w-full xl:w-[60%]' />
         </div>
       </section>
-      <section ref={currenciesSection} className='pt-20 relative on-scroll-transition'>
-        <div
-          className='sm:px-14 md:px-20 lg:px-20 py-12 lg:h-96 bg-secondary rounded-[2rem] lg:rounded-full 
-          flex items-center gap-6 justify-center flex-col lg:flex-row'
-        >
-          <CryptoCard
-            className='max-md:min-w-[100%] max-md:max-w-[100%] max-lg:min-w-[80%] max-lg:max-w-[80%] max-lg:rounded-[3rem]'
-            fullName='Bitcoin'
-            nameFrom='BTC'
-            nameTo='USD'
-            price={3213.315123}
-            iconName='btc'
-          />
-          <CryptoCard
-            className='max-md:min-w-[100%] max-md:max-w-[100%] max-lg:min-w-[80%] max-lg:max-w-[80%] max-lg:rounded-[3rem]'
-            fullName='Bitcoin'
-            nameFrom='BTC'
-            nameTo='USD'
-            price={123323.49}
-            iconName='eth'
-          />
-          <CryptoCard
-            className='2xl:block lg:hidden max-md:min-w-[100%] max-md:max-w-[100%] max-lg:min-w-[80%] max-lg:max-w-[80%] max-lg:rounded-[3rem]'
-            fullName='Bitcoin'
-            nameFrom='BTC'
-            nameTo='USD'
-            price={3123123.49}
-            iconName='btc'
-          />
-          <CryptoCard
-            className='4xl:block lg:hidden max-md:min-w-[100%] max-md:max-w-[100%] max-lg:min-w-[80%] max-lg:max-w-[80%] max-lg:rounded-[3rem]'
-            fullName='Bitcoin'
-            nameFrom='BTC'
-            nameTo='USD'
-            price={31231.49}
-            iconName='btc'
-          />
-        </div>
-      </section>
-      <section id='what-is-cryptonus' className='my-8 mx-2 sm:mx-6 md:mx-12 bg-primary-dark/10 dark:bg-primaryDark-dark rounded-xl md:py-4  xl:py-16 px-6'>
-        <div className='flex gap-16 flex-col xl:flex-row'>
-          <div className='basis-2/5'>
-            <img src="assets/images/crypto-home-globe.png" alt="" className='m-auto w-[70%] xl:w-fit xl:max-h-[650px]' />
-          </div>
-          <div ref={whatIsCryptonusRef} className='basis-3/5 flex flex-col on-scroll-transition'>
-            <h2 className='text-2xl xs:text-3xl sm:text-5xl md:text-6xl'>What is Cryptonus</h2>
-            <p className='xs:text-md sm:text-xl md:text-2xl opacity-85 my-6'>Receive the best trading experience with Cryptonus. Many features are available for you. Start now and receive a bonus!</p>
-            {
-              [
-                'Access real-time cryptocurrency prices',
-                'Buy & Sell the most popular cryptocurrencies',
-                'P2P trading available',
-                '24/7 Customer support'
-              ].map(text =>
-              (
-                <div key={text} className='flex items-center xs:text-xl  sm:text-2xl md:text-3xl my-4'><ChevronRight size={40} />{text}</div>
-              ))
-            }
-          </div>
-        </div>
-      </section>
+      <CurrenciesSection />
+      <WhatIsCryptonus />
     </div>
   )
 }
