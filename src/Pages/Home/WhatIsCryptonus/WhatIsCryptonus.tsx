edd1bc4c0ada06558e9505310803ce18a0a4c999
Svetoslav Borislavov <39881430+Svetoslavb04@users.noreply.github.com>
2022-10-09 01:04:26 +0300

    Moved WhatIsCryptonus Component

--- src/Pages/Home/WhatIsCryptonus/WhatIsCryptonus.tsx
+++ src/Pages/Home/WhatIsCryptonus/WhatIsCryptonus.tsx
@@ -1,5 +1,5 @@
 import { FC, useRef } from 'react'
-import { useScrollTransition } from '../../hooks/useScrollTransition';
+import { useScrollTransition } from '../../../hooks/useScrollTransition';
 
 import { ChevronRight } from 'react-feather';
 
@@ -7,10 +7,10 @@ const WhatIsCryptonus: FC = () => {
 
     const whatIsCryptonusRef = useRef<HTMLDivElement>(null);
 
-    useScrollTransition(whatIsCryptonusRef, 50);
+    useScrollTransition(whatIsCryptonusRef, 80);
 
     return (
-        <section id='what-is-cryptonus' className='my-8 mx-2 sm:mx-6 md:mx-12 bg-primary-dark/10 dark:bg-primaryDark-dark rounded-xl md:py-4  xl:py-16 px-6'>
+        <section id='what-is-cryptonus' className='my-8 mx-2 sm:mx-6 md:mx-12 bg-primary-dark/10 dark:bg-primaryDark-dark rounded-xl md:py-4 xl:py-16 px-6'>
             <div className='flex gap-16 flex-col xl:flex-row'>
                 <div className='basis-2/5'>
                     <img src="assets/images/crypto-home-globe.png" alt="" className='m-auto w-[70%] xl:w-fit xl:max-h-[650px]' />
